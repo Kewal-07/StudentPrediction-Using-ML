35381e01665b26ad616208a22f83a0772524ac0f
“Kewal <dharamshikewal@gmail.com>
2024-11-23 18:10:26 +0530

    EDA and problem statement

--- src/exception.py
+++ src/exception.py
@@ -28,11 +28,5 @@ if __name__ == "__main__":
     except Exception as e:
         logging.info("Divide by zero error")
         raise CustomException(e,sys)
-    
-if __name__ =="__main__":
-    try:
-        a=1/0
-    except Exception as e:
-        logging.info("Divide by Zero")  # INFO
-        raise CustomException(e,sys)
+
     
